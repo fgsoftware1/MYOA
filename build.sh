8339956bcebf7fca15586592f71dfe1248ca3192
fgsoftware1 <fgsoftwarestudio@gmail.com>
2025-10-23 16:44:31 +0100

    Add .gitignore and update build

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 if [[ $EUID -eq 0 ]]; then
-    docker build -t 'myoa:latest' .
-    docker run --privileged -v ./profile:/profile 'myoa:latest'
+    docker run --privileged -v ./profile:/profile 'ghcr.io/fgsoftware1/myoa'
 else
     echo "Please run me as root!"
 fi   
